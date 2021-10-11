730ed26f9f5990a2a0252e74eaa6d1eb17c98a53
Faruk Cesur <faruk-cesur@hotmail.com>
2021-10-11 15:00:58 +0300

    Created Default Material For Bridge

--- Assets/GAME/Scripts/Scripts/Bridge.cs
+++ Assets/GAME/Scripts/Scripts/Bridge.cs
@@ -26,7 +26,7 @@ public class Bridge : MonoBehaviour
                 if (character.CompareTag("Blue") && GameManager.Instance.collectedBrickBlueList.Count > 0)
                 {
                     var go = Instantiate(gameObject, new Vector3(transform.localPosition.x, transform.localPosition.y + 0.30f, transform.localPosition.z + 0.6f), transform.localRotation, parentObject.transform);
-                    go.GetComponentInChildren<MeshRenderer>().sharedMaterial = go.GetComponent<Bridge>().materials[0];
+                    go.GetComponentInChildren<MeshRenderer>().sharedMaterial = go.GetComponent<Bridge>().materials[1];
                     Destroy(GameManager.Instance.collectedBrickBlueList[GameManager.Instance.collectedBrickBlueList.Count - 1]);
                     GameManager.Instance.collectedBrickBlueList.RemoveAt(GameManager.Instance.collectedBrickBlueList.Count - 1);
                     stairCollider.enabled = false;
