5aad9e2f8922b97854f6881dc6822c7125983183
Faruk Cesur <faruk-cesur@hotmail.com>
2021-10-11 14:27:16 +0300

    Added Stair Collider & Brick Color Changes By Player Color

--- Assets/GAME/Scripts/Scripts/Bridge.cs
+++ Assets/GAME/Scripts/Scripts/Bridge.cs
@@ -4,51 +4,15 @@ using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
 
-public enum BridgeColors
-{
-    Blue,
-    Green,
-    Pink,
-    Orange
-}
 
 public class Bridge : MonoBehaviour
 {
-    public int usedBridgeBlue = 0;
-    public int usedBridgeGreen = 0;
-    public int usedBridgePink = 0;
-    public int usedBridgeOrange = 0;
-    
-    
-
-    [OnValueChanged("CurrentColor")] public BridgeColors color;
-
     [ReorderableList] [SerializeField] private List<Material> materials;
 
-    [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private GameObject parentObject;
+    [SerializeField] private Collider stairCollider;
 
     private bool _isCharacterTouch;
-    
-    
-    private void CurrentColor()
-    {
-        switch (color)
-        {
-            case BridgeColors.Blue:
-                meshRenderer.sharedMaterial = materials[0];
-                break;
-            case BridgeColors.Green:
-                meshRenderer.sharedMaterial = materials[1];
-                break;
-            case BridgeColors.Pink:
-                meshRenderer.sharedMaterial = materials[2];
-                break;
-            case BridgeColors.Orange:
-                meshRenderer.sharedMaterial = materials[3];
-                break;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -58,12 +22,14 @@ public class Bridge : MonoBehaviour
             if (!_isCharacterTouch)
             {
                 _isCharacterTouch = true;
-                
+
                 if (character.CompareTag("Blue") && GameManager.Instance.collectedBrickBlueList.Count > 0)
                 {
-                    Instantiate(gameObject, new Vector3(transform.localPosition.x, transform.localPosition.y + 0.30f, transform.localPosition.z + 0.6f),transform.localRotation,parentObject.transform);
-                    Destroy(GameManager.Instance.collectedBrickBlueList[GameManager.Instance.collectedBrickBlueList.Count -1]);
-                    GameManager.Instance.collectedBrickBlueList.RemoveAt(GameManager.Instance.collectedBrickBlueList.Count-1);
+                    var go = Instantiate(gameObject, new Vector3(transform.localPosition.x, transform.localPosition.y + 0.30f, transform.localPosition.z + 0.6f), transform.localRotation, parentObject.transform);
+                    go.GetComponentInChildren<MeshRenderer>().sharedMaterial = go.GetComponent<Bridge>().materials[0];
+                    Destroy(GameManager.Instance.collectedBrickBlueList[GameManager.Instance.collectedBrickBlueList.Count - 1]);
+                    GameManager.Instance.collectedBrickBlueList.RemoveAt(GameManager.Instance.collectedBrickBlueList.Count - 1);
+                    stairCollider.enabled = false;
                 }
             }
         }
