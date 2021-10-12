6d8e0cb5469166fae142a2158a3ffdebc31b3b43
Faruk Cesur <faruk-cesur@hotmail.com>
2021-10-12 16:07:35 +0300

    Bricks Respawn When Use Them To Build Bridge

---
+++ Assets/GAME/Scripts/Scripts/BrickSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BrickSpawner : Singleton<BrickSpawner>
+{
+    public List<GameObject> bricksList;
+
+    public List<Vector3> bricksPositionList;
+
+    private void Awake()
+    {
+        foreach (var brick in bricksList)
+        {
+            bricksPositionList.Add(brick.transform.position);
+        }
+    }
+}
--- Assets/GAME/Scripts/Scripts/Bridge.cs
+++ Assets/GAME/Scripts/Scripts/Bridge.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class Bridge : MonoBehaviour
 {
     [ReorderableList] [SerializeField] private List<Material> materials;
-    [SerializeField] private GameObject parentObject;
+    [SerializeField] private GameObject bridgeParentObject;
+    [SerializeField] private GameObject brickParentObject;
     [SerializeField] private Collider stairCollider;
 
     private bool _isCharacterTouch;
@@ -23,13 +25,24 @@ public class Bridge : MonoBehaviour
                 if (character.CompareTag("Blue") && character.player.collectedBrickListBlue.Count > 0)
                 {
                     _isCharacterTouch = true;
-                    var go = Instantiate(gameObject, new Vector3(transform.localPosition.x, transform.localPosition.y + 0.30f, transform.localPosition.z + 0.6f), transform.localRotation, parentObject.transform);
+                    var go = Instantiate(gameObject, new Vector3(transform.localPosition.x, transform.localPosition.y + 0.30f, transform.localPosition.z + 0.6f), transform.localRotation, bridgeParentObject.transform);
                     go.GetComponentInChildren<MeshRenderer>().sharedMaterial = go.GetComponent<Bridge>().materials[1];
-                    Destroy(character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject);
-                    character.player.collectedBrickListBlue.RemoveAt(character.player.collectedBrickListBlue.Count - 1);
                     stairCollider.enabled = false;
+                    character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject.tag = "Empty";
+                    character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject.transform.SetParent(brickParentObject.transform);
+                    character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                    character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject.GetComponent<Collider>().enabled = true;
+                    for (int i = 0; i < BrickSpawner.Instance.bricksList.Count; i++)
+                    {
+                        if (BrickSpawner.Instance.bricksList[i].CompareTag("Empty"))
+                        {
+                            BrickSpawner.Instance.bricksList[i].tag = "Untagged";
+                            character.player.collectedBrickListBlue[character.player.collectedBrickListBlue.Count - 1].gameObject.transform.position = BrickSpawner.Instance.bricksPositionList[i];
+                        }
+                    }
+
+                    character.player.collectedBrickListBlue.RemoveAt(character.player.collectedBrickListBlue.Count - 1);
                 }
-                //character.player.collectedBrickListBlue.IndexOf()
             }
         }
     }
